2939c98ff8caccf7682dd3d19b527bc5611268f4
Ciel <danagames30@gmail.com>
2022-12-23 22:50:30 +0700

    [Backend] Update user migration

--- backend/migrations/20221223135051-create-user-model.js
+++ backend/migrations/20221223135051-create-user-model.js
@@ -47,6 +47,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserModels');
+    await queryInterface.dropTable('users');
   }
 };
