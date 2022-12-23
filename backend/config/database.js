2a60fdc512e0e64fcdcf71a69733646ea8a94542
Ciel <danagames30@gmail.com>
2022-12-23 19:14:44 +0700

    [Backend] update env example and config/database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('wargapedulilingkungan', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
 })
 
 export default db;
